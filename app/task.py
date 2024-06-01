833755e3c8772de403b4c0d525497be488955a60
officialryder1~ <kennethvic07@gmail.com>
2024-06-01 09:28:32 -0700

    change db

--- app/task.py
+++ app/task.py
@@ -1,31 +1,18 @@
-from replicate.client import Client
-from django.conf import settings
-client = Client(api_token=settings.REPLICATE_API_TOKEN)
+from concurrent.futures import as_completed
+from django_thread import ThreadPoolExecutor
 
-def tweet(x: str):
-    stream = client.stream(
-    ref = "meta/meta-llama-3-70b-instruct",
-        input={
-        "top_k": 0,
-        "top_p": 0.9,
-        "prompt": "rephrase this tweet for me, just the rephrase no intro " + str(x),
-        "max_tokens": 512,
-        "min_tokens": 0,
-        "temperature": 0.6,
-        "system_prompt": "You are a helpful assistant",
-        "length_penalty": 1,
-        "stop_sequences": "<|end_of_text|>,<|eot_id|>",
-        "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
-        "presence_penalty": 1.15,
-        "log_performance_metrics": False
-        },
 
-    )
+def update_or_create_thing(name):
+    thing, _ = Thing.objects.update_or_create(name=name)
+    return thing.id
 
-    full_response = []
-    for chunk in stream:
-        full_response.append(chunk)
 
-    output_respond = []
-    for output in full_response:
-        output_respond.append(output)
+executor = ThreadPoolExecutor()
+
+futures = []
+for i in range(5):
+    future = executor.submit(update_or_create_thing, f'Name i')
+    futures.append(future)
+
+ids = [f.result() for f in futures]
+
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from replicate.client import Client
 from .models import Tweets, UserToken, Subscription, user_subscription
 from django.contrib import messages
-from .task import tweet
+
 
 client = Client(api_token=settings.REPLICATE_API_TOKEN)
 
@@ -75,7 +75,6 @@ def search_tweet(request):
     token = UserToken.objects.get(user=request.user)
     token.token -= 1
     token.save()
-    print(output_respond)
     return render(request, 'partials/tweet_list.html', {'event':full_response})
   
 
@@ -203,36 +202,4 @@ def rephrase_list(request):
   return render(request, 'partials/rephrase_list.html', {'event':full_response} )
 
 def next(request):
-  tweet = request.POST.get('tweet')
-
-  stream = client.stream(
-    ref = "meta/meta-llama-3-70b-instruct",
-     input={
-        "top_k": 0,
-        "top_p": 0.9,
-        "prompt": "rephrase this tweet for me, just the rephrase no intro " + str(tweet),
-        "max_tokens": 512,
-        "min_tokens": 0,
-        "temperature": 0.6,
-        "system_prompt": "You are a helpful assistant",
-        "length_penalty": 1,
-        "stop_sequences": "<|end_of_text|>,<|eot_id|>",
-        "prompt_template": "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
-        "presence_penalty": 1.15,
-        "log_performance_metrics": False
-     },
-    
-  )
-
-  full_response = []
-  for chunk in stream:
-    full_response.append(chunk)
-
-  output_respond = []
-  for output in full_response:
-    output_respond.append(output)
-
-  token = UserToken.objects.get(user=request.user)
-  token.token -= 2
-  token.save()
-  return render(request, 'partials/rephrase_list.html', {'event':full_response} )
+  pass
